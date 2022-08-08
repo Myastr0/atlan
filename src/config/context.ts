a2c7a5782bdec5383bdeb123aa42b8265e7e5ab3
Myastro <dumon.leo@gmail.com>
2022-08-08 21:13:52 +0200

    build(upgrade version): upgrade version

--- src/config/context.ts
+++ src/config/context.ts
@@ -3,5 +3,5 @@ import core from './core';
 export default {
   metafileName: 'atlan-ctx.json',
   dirPath: core.dirPath + '/contexts',
-  templateContextsDirPath: __dirname + '/../../context-templates',
+  templateContextsDirPath: __dirname + '/../context-templates',
 };
